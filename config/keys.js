80110e091caa56dc0eaed472cf288d30c590820d
Saad <saad_anwer@live.com>
2020-05-04 21:59:05 +0500

    init

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+
+                          //your name:your password @clustername    
+dbPassword = 'mongodb+srv://Saad:saad123@devconnector-4a3xi.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
